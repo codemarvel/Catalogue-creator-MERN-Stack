68708ab19b32e678c565586570e022526da3ca00
Mayank Saini <sainimayank70@gmail.com>
2021-01-11 19:21:13 +0530

    database fixes

--- database/db.js
+++ database/db.js
@@ -1,4 +1,3 @@
 module.exports = {
-  /*db: 'mongodb://localhost:27017/reactdb'*/
-  MONGODB_URI: 'mongodb+srv://mayank:mayank4321@cluster0.wvqky.mongodb.net/catalogue?retryWrites=true&w=majority'
+  db: 'mongodb://localhost:27017/reactdb' 
 };
